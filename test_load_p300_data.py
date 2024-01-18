1eb32d1dc0db86f3e1d5c7c653096c2bf60d002b
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2024-01-18 12:41:55 -0500

    Started adding comments

--- test_load_p300_data.py
+++ test_load_p300_data.py
@@ -1,3 +1,8 @@
+"""
+Created on Wed Jan 17, 5:50pm 2024
+
+@author: magallo
+"""
 #%%
 
 from load_p300_data import load_training_eeg, plot_raw_eeg
@@ -6,5 +11,7 @@ from load_p300_data import load_training_eeg, plot_raw_eeg
 data_directory = './BCIs_S24/P300Data'
 subject = 3
 
+# Load training data into variables from passing our subject and data directory into load_training_eeg()
 eeg_time, eeg_data, rowcol_id, is_target = load_training_eeg(subject, data_directory)
+# Use our predefined function and variables to plot the variables
 plot_raw_eeg(subject, eeg_time, eeg_data, rowcol_id, is_target)
