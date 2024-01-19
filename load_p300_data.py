97b4a2bde5d583fb83f16f46a14d5bbad29eb25a
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2024-01-18 19:11:13 -0500

    Finished Parts 6-7

--- load_p300_data.py
+++ load_p300_data.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """
 Created on Wed Jan 17, 5:50pm 2024
-
 @author: magallo
+
+This module contains three functions:
+    load_training_eeg(subject, data_directory): Loads training data from filename calculated with the subject number and given data_directory.
+    plot_raw_eeg(subject, eeg_time, eeg_data, rowcol_id, is_target): Plots parameters on a 3x1 matplotlib display with subject being displayed in the title, eeg_time as the x-axis in each plot, and the remaining parameters (excluding subject) plotted on the y-axis for each graph.
+    load_and_plot_all(data_directory, subjects): Loads training data from filename calculated with the subject number and given data_directory.
 """
 #%%
 
@@ -10,6 +14,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import BCIs_S24.loadmat as loadmat
 
+
+#%%
 def load_training_eeg(subject, data_directory):
     """Loads training data from filename calculated with the subject number and given data_directory.
     Args:
@@ -17,10 +23,10 @@ def load_training_eeg(subject, data_directory):
         data_directory (string): string representing the file path from script directory execution to data files.
     
     Returns:
-        np.array: array containing the time marker.
-        np.array: array containing a list of multiple inputs for each element
-        np.array (int): array of integers representing the row/column identifier for this data.
-        np.array (bool): array of booleans representing whether or not this data""" 
+        np.array: array containing the time marker for each datapoint.
+        np.array: array containing a list of multiple eeg inputs for each datapoint.
+        np.array <int>: array of integers representing the flashed row/column identifier being flashed for each datapoint.
+        np.array <bool>: array of booleans representing whether or not the flashed row/column contains the target letter for each datapoint""" 
     # Load data, extract training data
     data_file = f"{data_directory}/s{subject}.mat"
     data = loadmat.loadmat(data_file)
@@ -36,10 +42,19 @@ def load_training_eeg(subject, data_directory):
     
     return eeg_time, eeg_data, rowcol_id, is_target
 
+
 def plot_raw_eeg(subject, eeg_time, eeg_data, rowcol_id, is_target):
+    """Plots parameters on a 3x1 matplotlib display with subject being displayed in the title, eeg_time as the x-axis in each plot, and the remaining parameters (excluding subject) plotted on the y-axis for each graph.
+    Args:
+        subject (int): integer representing the subject number.
+        eeg_time (np.array): array containing the time marker for each datapoint.
+        eeg_data (np.array): array containing a list of multiple eeg inputs for each datapoint.
+        rowcol_id (np.array <int>): array of integers representing the flashed row/column identifier being flashed for each datapoint.
+        is_target (np.array <bool>): array of booleans representing whether or not the flashed row/column contains the target letter for each datapoint""" 
     fig, axs = plt.subplots(3, 1, sharex=True)
     fig.suptitle(f"P300 Speller subject {subject} Raw Data")
-    # Time Display Range
+    
+    # Scope the ranges displayed on the x-axis (eeg_time)
     time_x_min = 48
     time_x_max = 53
 
@@ -47,6 +62,7 @@ def plot_raw_eeg(subject, eeg_time, eeg_data, rowcol_id, is_target):
     axs[0].plot(eeg_time, rowcol_id)
     axs[0].set_xlabel('time (s)')
     axs[0].set_ylabel('row/col ID')
+    # Scope x-axis
     axs[0].set_xlim(time_x_min, time_x_max)
     axs[0].grid(True)
 
@@ -54,6 +70,7 @@ def plot_raw_eeg(subject, eeg_time, eeg_data, rowcol_id, is_target):
     axs[1].plot(eeg_time, is_target)
     axs[1].set_xlabel('time (s)')
     axs[1].set_ylabel('Target ID')
+    # Scope x-axis
     axs[1].set_xlim(time_x_min, time_x_max)
     axs[1].grid(True)
 
@@ -61,10 +78,11 @@ def plot_raw_eeg(subject, eeg_time, eeg_data, rowcol_id, is_target):
     axs[2].plot(eeg_time, eeg_data.T)
     axs[2].set_xlabel('time (s)')
     axs[2].set_ylabel('Voltage (uV)')
+    # Scope our plot
     axs[2].set_xlim(time_x_min, time_x_max)
     axs[2].set_ylim(-25, 25)
     axs[2].grid(True)
-
+    
     plt.tight_layout()
 
     # Save plot to file
@@ -72,3 +90,21 @@ def plot_raw_eeg(subject, eeg_time, eeg_data, rowcol_id, is_target):
 
     # Show plot
     plt.show()
+  
+
+#%%
+
+def load_and_plot_all(data_directory, subjects):
+    """Loads training data from filename calculated with the subject number and given data_directory.
+    Args:
+        data_directory (string): string representing the file path from script directory execution to data files.
+        subject (List <int>): integer representing the subject number.
+    Returns:
+        None"""
+    # Iterate through each subject
+    for subject in subjects:
+        # Load training data into variables from passing our subject and data directory into load_training_eeg()
+        eeg_time, eeg_data, rowcol_id, is_target = load_training_eeg(subject, data_directory)
+        # Use our plotting function and our new variables to plot the variables
+        plot_raw_eeg(subject, eeg_time, eeg_data, rowcol_id, is_target)
+    
--- test_load_p300_data.py
+++ test_load_p300_data.py
@@ -1,17 +1,33 @@
 """
 Created on Wed Jan 17, 5:50pm 2024
-
 @author: magallo
+
+This module has 3 cells, which implement methods created in sibling file `load_p300_data.py`:
+    Cell 1: Tests our functions for handling of loading raw data, then plotting raw data.
+    Cell 2: Tests our function for handling of loading raw data, then plotting raw data for multiple subjects.
+    Cell 3: Contains my solution for determining the word that the test subjects were told to spell.
 """
-#%%
+#%% Cell 1
 
 from load_p300_data import load_training_eeg, plot_raw_eeg
 
-# Define our 'magic numbers'
 data_directory = './BCIs_S24/P300Data'
 subject = 3
 
 # Load training data into variables from passing our subject and data directory into load_training_eeg()
 eeg_time, eeg_data, rowcol_id, is_target = load_training_eeg(subject, data_directory)
-# Use our predefined function and variables to plot the variables
+# Use our plotting function and our new variables to plot the variables
 plot_raw_eeg(subject, eeg_time, eeg_data, rowcol_id, is_target)
+
+#%%
+
+from load_p300_data import load_and_plot_all
+
+data_directory = './BCIs_S24/P300Data'
+# Create our subjects list of ints 3-10
+subjects = range(3,11)
+# Load all of our subjects' data and plot them
+load_and_plot_all(data_directory, subjects)
+
+#%%
+
