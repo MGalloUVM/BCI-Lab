d9051babee2340f718340328df0c1272d0a29585
Apc-03 <118464923+Apc-03@users.noreply.github.com>
2024-02-05 21:08:47 -0500

    Update test_plot_p300_erps.py
    Calls functions from part 4&5, added file docstring at top as template

--- test_plot_p300_erps.py
+++ test_plot_p300_erps.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 25 13:13:18 2024
+Provides functions for loading and extracting P300 data, plotting the resulting data for multiple subjects,
+and printing participants spelling and word count.
 
+Functions contain utility for extracting and slicing data from a data file, creating subplots with shared x axes,
+looping through and plotting multiple instances, and predicting desired letters based on an RC speller.
+
+@author: Aiden Pricer-Coan
 @author: mike
+
+file: test_plot_p300_erps.py
+BME 6710 - Jangraw
+Lab Two: Event_Related Potentials
 """
+
+# import statements
 #%% Part 1
 from load_p300_data import load_training_eeg
 
@@ -28,3 +39,9 @@ event_sample, is_target_event = plot_p300_erps.get_events(rowcol_id, is_target)
 
 
 eeg_epochs, erp_times = plot_p300_erps.epoch_data(eeg_time, eeg_data, event_sample)
+
+# Get mean ERPs for target and non-target events
+target_erp, nontarget_erp = plot_p300_erps.get_erps(eeg_epochs, is_target_event)
+
+# Plot ERPs of the subject
+plot_p300_erps.plot_erps(target_erp, nontarget_erp, erp_times)
