6432eb00e39636d827d39f22f02999eeff24b862
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2024-02-07 20:06:48 -0500

    Significant comment refactoring, added additional thoughts to Part 6

--- plot_p300_erps.py
+++ plot_p300_erps.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Provides functions for loading and extracting P300 data, plotting the resulting data for multiple subjects,
-and printing participants spelling and word count.
+This file's purpose is processing and analyzing EEG data.
 
-Functions contain utility for extracting and slicing data from a data file, creating subplots with shared x axes,
-looping through and plotting multiple instances, and predicting desired letters based on an RC speller.
+Part 1: Setup
+  Import necessary Python packages
+
+Part 2: Event Detection
+  Define a function to determine the start of stimulus events; categorize them as target/non-target events.
+
+Part 3: Exploratory Data Analysis
+  Functions to establish sampling rate and to segment EEG data into epochs.
+  Constants defining sample rate and channel count.
+
+Part 4: ERP Extraction
+    Extract the event-related potentials (ERPs) for target and non-target events.
+
+Part 5: Visualization
+  Plotting functions for the visualization of ERP averages across EEG channels.
 
 @author: Aiden Pricer-Coan
-@author: mike
+@author: Michael Gallo
 
 file: plot_p300_erps.py
 BME 6710 - Jangraw
@@ -28,12 +40,12 @@ def get_events(rowcol_id, is_target):
     given data_directory.
     
     Args:
-        rowcol_id (np.array <int>): array of integers representing the flashed row/column identifier being flashed for each datapoint.
-        is_target (np.array <bool>): array of booleans representing whether or not the flashed row/column contains the target letter for each datapoint
+        rowcol_id <np.array (int)>[TOTAL_SAMPLES]: array of integers representing the flashed row/column identifier being flashed for each datapoint.
+        is_target <np.array (bool)>[TOTAL_SAMPLES]: array of booleans representing whether or not the flashed row/column contains the target letter for each datapoint
     
     Returns:
-        np.array <int>: indices where every new event begins.
-        np.array <bool>: array where each element indicates whether an event was a target event or not.
+        event_sample <np.array (int)>[TOTAL_SAMPLES]: indices where every new event begins.
+        is_target_event <np.array (bool)>[TOTAL_SAMPLES]: array where each element indicates whether an event was a target event or not.
     """
     # Calculate the difference between rowcol_id vals
     diff_rowcol_id = np.diff(rowcol_id)
@@ -55,10 +67,10 @@ def num_samples_in_sec(eeg_time):
     Used to find the number of samples per second of data.
     
     Args:
-        eeg_time (np.array): array of floats representing the time value in seconds at each index.
+        eeg_time <np.array (float)>: array of floats representing the time value in seconds at each index.
     
     Returns:
-        int: number of elements within 1s of data
+        time_index <int>: number of elements within 1s of data
     """
     first_time = eeg_time[0]
     time_index = 1
@@ -69,6 +81,8 @@ def num_samples_in_sec(eeg_time):
     return time_index
 
 #%% Part 3
+
+# Calculated above AND confirmed in documentation
 SAMPLES_PER_SECOND = 256
 NUM_CHANNELS = 8
 
@@ -77,16 +91,16 @@ def epoch_data(eeg_time, eeg_data, event_sample, epoch_start_time=-0.5, epoch_en
     Loads our data into epoch blocks for further analysis.
     
     Args:
-        eeg_time (np.array): array of floats representing the time value in seconds at each index.
-        eeg_data (np.array <np.array>): 2d array with EEG values for each channel at every data point in the experiment data.
-            - NOTE: eeg_data[i][j], where i represents the i(th) channel
+        eeg_time <np.array>[TOTAL_SAMPLES] : array of floats representing the time value in seconds at each index.
+        eeg_data <np.array>[TOTAL_SAMPLES, NUM_CHANNELS] : 2d array with EEG values for each channel at every data point in the experiment data.
+            - NOTE: eeg_data[i, j], where i represents the i(th) channel
                                           j represents the j(th) element
-        event_sample (np.array <int>): indices where every new event begins.
-        epoch_start_time <float>: start time offset from start point of each epoch, can be + or -
-        epoch_end_time <float>: end time offset from start point of each epoch, should be > epoch_start_time
+        event_sample <np.array (int)>[NUM_EPOCHS] : indices where every new event begins.
+        epoch_start_time <float> : start time offset from start point of each epoch, can be + or -
+        epoch_end_time <float> : end time offset from start point of each epoch, should be > epoch_start_time
 
     Returns:
-        eeg_epochs (np.array <np.array...>): 3d array representing an epoch for each event in our data
+        eeg_epochs <np.array>[NUM_EPOCHS, TOTAL_SAMPLES, NUM_CHANNELS]: 3d array representing an epoch for each event in our data
             - eeg_epochs[i][j][k], where i represents the i(th) epoch, 
                                          j represents the j(th) sample in the epoch,
                                          k represents the k(th) channel of data in the epoch.
@@ -112,7 +126,7 @@ def epoch_data(eeg_time, eeg_data, event_sample, epoch_start_time=-0.5, epoch_en
         window_end_time = event_start_time + epoch_end_time
         
         # Get indices within window...
-        window_indices = np.where((eeg_time > window_start_time) & (eeg_time <= window_end_time))[0]
+        window_indices = np.where((eeg_time >= window_start_time) & (eeg_time < window_end_time))[0]
         # Get epoch data, transpose because (eeg_data[i][j])'s i represents channel, but we NEED i to represent the sample index, and j the channel
         epoch_data = eeg_data[:, window_indices].T
         # Set the epoch data
@@ -124,6 +138,7 @@ def epoch_data(eeg_time, eeg_data, event_sample, epoch_start_time=-0.5, epoch_en
 
     return eeg_epochs, erp_times
 
+#%% Part 4
 
 def get_erps(eeg_epochs, is_target_event):
     """
@@ -134,8 +149,8 @@ def get_erps(eeg_epochs, is_target_event):
         is_target_event (np.array <bool>): array indicating whether each event is a target event or not
 
     Returns:
-        np.array[NUM_EPOCHS] <np.array[8]<float>>: ERPs for target events
-        np.array[NUM_EPOCHS] <np.array[8]<float>>: ERPs for non-target events
+        np.array[NUM_EPOCHS] <np.array (float)>[NUM_CHANNELS] : ERPs for target events
+        np.array[NUM_EPOCHS] <np.array (float)>[NUM_CHANNELS] : ERPs for non-target events
     """
     # Select epochs corresponding to target events
     target_epochs = eeg_epochs[is_target_event]
@@ -149,15 +164,16 @@ def get_erps(eeg_epochs, is_target_event):
 
     return target_erp, nontarget_erp
 
+#%% Part 5
 
 def plot_erps(target_erp, nontarget_erp, erp_times):
     """
     Plots event-related potentials (ERPs) for target and non-target events.
 
     Args:
-        target_erp (np.array): Array containing the mean response on each channel for target events.
-        nontarget_erp (np.array): Array containing the mean response on each channel for non-target events.
-        erp_times (np.array): Array representing the time difference of each datapoint from the start of the event.
+        target_erp (np.array)[NUM_EPOCHS]: Array containing the mean response on each channel for target events.
+        nontarget_erp (np.array)[NUM_EPOCHS]: Array containing the mean response on each channel for non-target events.
+        erp_times (np.array)[NUM_EPOCHS]: Array representing the time difference of each datapoint from the start of the event.
 
     Returns:
         None
@@ -173,9 +189,6 @@ def plot_erps(target_erp, nontarget_erp, erp_times):
         
         # Plot nontarget ERP for this channel
         axes[channel_number].plot(erp_times, nontarget_erp[:, channel_number], label='Non-target')
-        
-        # Set y range
-        axes[channel_number].set_ylim(-2, 2)
         
         # Add reference lines at x=0 and y=0
         axes[channel_number].axhline(0, color='black', linestyle='dotted')
