f09b569b6c975cd715f163028b89917c75e9678c
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2024-02-29 13:23:34 -0500

    Added description for part 3

--- import_ssvep_data.py
+++ import_ssvep_data.py
@@ -22,12 +22,41 @@ def load_ssvep_data(subject, data_directory):
     '''
     # Load information from np file into dictionary
     data_dict = np.load(f'{data_directory}/SSVEP_S{subject}.npz')
-    dict = {}
-    dict['eeg'] = data_dict['eeg']
     return data_dict
 
 
 #%% Part 2
 
-# loop with plt.plot([start_time,end_time],[event_type,event_type]) 
-# plot_raw_data(data, subject, channels_to_plot)
+def plot_raw_data(data, subject, channels_to_plot):
+    '''
+    FUNCTION DESCRIPTION
+    Inputs:
+     - data <> : DESCRIPTION
+     - subject <int> : Subject number, as shown in file title (Ex: SSVEP_S1, 1 is the subject number)
+     - channels_to_plot <list/array> : DESCRIPTION 
+    Outputs:
+     - None (Plots figures)
+    '''
+    # loop with plt.plot([start_time,end_time],[event_type,event_type]) 
+    # plot_raw_data(data, subject, channels_to_plot)
+    pass
+
+#%% Part 3
+
+def epoch_ssvep_data(data_dict, epoch_start_time=0, epoch_end_time=20):
+    '''
+    FUNCTION DESCRIPTION
+    Inputs:
+     - data_dict <> : DESCRIPTION
+     - epoch_start_time <float> : Desired start time of epoch relative to the event, in seconds.
+     - epoch_end_time <float> : Desired end time of epoch relative to the event, in seconds.
+    Outputs:
+     - eeg_epochs[x, y, z] <np.array> : EEG data after each epoch (in uV)
+        x[] <> : trials
+        y[] <> : channels
+        z[] <> : time
+     - epoch_times[x] <> : The time in seconds (relative to the event) of each time point in eeg_epochs
+        x : DESCRIBE
+     - is_trial_15Hz <> : DESCRIBE
+    '''
+    pass
